bcfc7678ace60e77768f1e17b9dbb91274ad50d9
jjcauchy-afk <jjcauchy@gmail.com>
2026-03-01 14:47:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+# Set page config
+st.set_page_config(
+    page_title="ISOM5580 Project",
+    page_icon = "",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+
+# Main title
+st.title("ISOM5580 Project")
+st.divider()
